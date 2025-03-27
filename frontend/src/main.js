b13dfa88bc9230d5934e984124b1a3fedf6e66cd
nydo(mac) <2337315348@qq.com>
2025-03-28 10:47:58 +1100

    followed with live lecture, worked on 2.1.2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,3 +3,19 @@ import { BACKEND_PORT } from './config.js';
 import { fileToDataUrl } from './helpers.js';
 
 console.log('Let\'s go!');
+
+document.getElementById('btn-register').addEventListener('click', () => {
+    const email = document.getElementById('register-email').value
+    const name = document.getElementById('register-name').value
+    const password = document.getElementById('register-password').value
+    const passwordConfirm = document.getElementById('register-password-confirm').value
+    console.log(
+        email,
+        name,
+        password,
+        passwordConfirm
+    )
+    if (password !== passwordConfirm) {
+        alert('hello')
+    }
+})
